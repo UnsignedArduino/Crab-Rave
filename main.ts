d1179f683a3a21cecff6294a9b8afe96ec6b3dc8
Ckyiu <UnsignedArduino@outlook.com>
2021-10-18 09:05:54 -0400

    Make skeleton functions and add extensions

--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
-
+function part_1 () {
+	
+}
+function part_2 () {
+	
+}
+function main2 () {
+	
+}
+function end () {
+	
+}
+function transition_2_main () {
+	
+}
+function transition_1_2 () {
+	
+}
+function transition_main_beginning () {
+	
+}
+function beginning () {
+	
+}
+function transition_main2_end () {
+	
+}
+function transition_2_main2 () {
+	
+}
+function transition_beginning_1 () {
+	
+}
+function main () {
+	
+}

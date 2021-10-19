a04d6b830107f94c51580df2189c856c763947be
Ckyiu <UnsignedArduino@outlook.com>
2021-10-19 17:42:02 -0400

    Include code for all of song parts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+function get_part (part: number) {
+    if (part == 1 || part == 6) {
+        return assets.animation`beginning`
+    } else if (part == 2 || part == 7) {
+        return assets.animation`beginning to part 1`
+    } else if (part == 3 || part == 8) {
+        return assets.animation`part 1`
+    } else if (part == 4 || part == 9) {
+        return assets.animation`part 1 to chorus`
+    } else if (part == 5) {
+        return assets.animation`chorus`
+    } else if (part == 10) {
+        return assets.animation`part 1 to chorus 2`
+    } else if (part == 11) {
+        return assets.animation`chorus 2`
+    } else if (part == 12) {
+        return assets.animation`chorus 2 to end`
+    } else if (part == 13) {
+        return assets.animation`end`
+    } else if (part == 14) {
+        return assets.animation`very end`
+    } else {
+        return []
+    }
+}
 function make_player () {
     sprite_player = sprites.create(assets.image`crab_idle_left`, SpriteKind.Player)
     tiles.placeOnTile(sprite_player, tiles.getTileLocation(4, 5))
@@ -6,8 +31,15 @@ function make_player () {
 function setup () {
     scene.setBackgroundColor(9)
     tiles.setTilemap(tilemap`map`)
+    musical = MusicalImages.create_musical_image()
     make_player()
 }
 let sprite_player: Sprite = null
+let musical: MusicalImages.MusicalImage = null
 stats.turnStats(true)
 setup()
+for (let index = 0; index <= 13; index++) {
+    MusicalImages.set_queue(musical, get_part(index + 1))
+    sprite_player.sayText("Part " + (index + 1))
+    MusicalImages.play(musical)
+}

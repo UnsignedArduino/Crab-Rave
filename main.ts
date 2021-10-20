3cc2968d4de8618a59d3d6da06f65336759f3bd8
Ckyiu <UnsignedArduino@outlook.com>
2021-10-20 08:14:55 -0400

    Make more UI stuff

--- main.ts
+++ main.ts
@@ -1,11 +1,27 @@
+namespace SpriteKind {
+    export const RhythmStuff = SpriteKind.create()
+}
 function part_1 () {
 	
+}
+function set_score (s: number) {
+    score = s
+    update_score()
 }
 function fade_out (block: boolean) {
     color.startFade(color.Black, color.originalPalette, 1000)
     if (block) {
         color.pauseUntilFadeDone()
     }
+}
+function update_score () {
+    if (!(sprite_score)) {
+        sprite_score = textsprite.create("", 0, 15)
+        sprite_score.top = 4
+        sprite_score.setFlag(SpriteFlag.Ghost, true)
+    }
+    sprite_score.setText("Score: " + score)
+    sprite_score.x = scene.screenWidth() / 2
 }
 function get_part_music (part: number) {
     if (part == 1 || part == 6) {
@@ -36,13 +52,20 @@ function make_player () {
     sprite_player = sprites.create(assets.image`crab_idle_left`, SpriteKind.Player)
     tiles.placeOnTile(sprite_player, tiles.getTileLocation(4, 5))
     sprite_player.x += tiles.tileWidth() / 2
+    sprite_player.setFlag(SpriteFlag.Ghost, true)
+}
+function make_rhythm_stuff () {
+    sprite_rhythm_bar = sprites.create(assets.image`rhythm_bar`, SpriteKind.RhythmStuff)
+    sprite_rhythm_bar.x = scene.screenWidth() / 2
+    sprite_rhythm_bar.top = 16
 }
 function setup () {
     scene.setBackgroundColor(9)
     tiles.setTilemap(tilemap`map`)
-    info.setScore(0)
+    set_score(0)
     musical = MusicalImages.create_musical_image()
     make_player()
+    make_rhythm_stuff()
 }
 function run_part (part: number) {
     if (part < 1 && part > 14) {
@@ -56,8 +79,15 @@ function run_part (part: number) {
     MusicalImages.set_queue(musical, get_part_music(part))
     MusicalImages.play(musical)
 }
+function change_score (s: number) {
+    score += s
+    update_score()
+}
 let musical: MusicalImages.MusicalImage = null
+let sprite_rhythm_bar: Sprite = null
 let sprite_player: Sprite = null
+let sprite_score: TextSprite = null
+let score = 0
 color.setPalette(
 color.Black
 )
